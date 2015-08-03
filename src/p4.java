45b4184ceaf37114aecb4bc52f908342c5d64f99
SHEEPKI <104021001@live.alsa.edu.tw>
2015-08-03 15:21:02 +0800

    第一個程式

---
+++ src/p4.java
@@ -0,0 +1,28 @@
+
+public class p4 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(" 　　　  　╭────────────╮");
+		System.out.println(" 　　　　│                 │");
+		System.out.println(" 　　　　│═════☉═══☉════│");
+		System.out.println("  　　　　│                │");
+		System.out.println("　　　　│                │");
+		System.out.println("　╭─　│　　　　　　　　　　　 　 │ ╮");
+		System.out.println("  　│　　│                │ │");
+		System.out.println("  　│ │ ▆▆▆▆▆▆▆▆│ │　　");
+		System.out.println("  　│ │ ▆▆▆▆▆▆▆▆│ │　　");
+		System.out.println("  　  ╰╯           ▆▆▆▆▆▆▆▆▆　　╰╯");
+		System.out.println("  　　 　     ▆▆▆▆▆▆▆▆▆▆▆▆▆　  　");
+		System.out.println("  　　 　                │ │     │ │　  　");
+		System.out.println("  　　 　                │ │     │ │　  　");
+		System.out.println("  　　 　                ║ ║     ║ ║ 　");
+		System.out.println("  　　 　               ╭─╮               ╭─╮ 　");
+		System.out.println("  　　 　               ╰─╯               ╰─╯　");
+		
+	}
+
+}
